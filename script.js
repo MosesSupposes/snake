e922bc423238551b10c99575d02ade8dd84fb958
Moses Samuel <mosamuel98@gmail.com>
2017-06-30 18:58:31 -0700

    create snake

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+var s;
 function setup() {
     createCanvas(600,600);
+    s = new Snake();
 }
 
 function draw() {
     background(51);
+    s.update();
+    s.show();
+}
+
+// Constructor
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+
+    this.update = function() {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+
+    this.show = function() {
+        fill(255) // white
+        rect(this.x, this.y, 10,10)
+    }
 }
