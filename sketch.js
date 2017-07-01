12a8b8ef0ed23cd4c5d530eb0f0bc83c5a845131
Moses Samuel <mosamuel98@gmail.com>
2017-06-30 19:36:23 -0700

    set constraints on x and y axes + signficantly slow down frame rate

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 var s;
+var scl = 20;
+
 function setup() {
     createCanvas(600,600);
     s = new Snake();
+    frameRate(10);
 }
 
 function draw() {
--- snake.js
+++ snake.js
@@ -5,18 +5,22 @@ function Snake() {
     this.xspeed = 1;
     this.yspeed = 0;
 
-    this.update = function() {
-        this.x += this.xspeed;
-        this.y += this.yspeed;
-    }
-
-    this.show = function() {
-        fill(255) // white
-        rect(this.x, this.y, 10,10)
-    }
-
     this.dir = function(x, y) {
         this.xspeed = x;
         this.yspeed = y;
     }
+
+    this.update = function() {
+        this.x += this.xspeed * scl;
+        this.y += this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    }
+
+    this.show = function() {
+        fill(255) // white
+        rect(this.x, this.y, scl, scl);
+    }
+
 }
