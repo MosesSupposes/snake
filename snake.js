eee56959b87a41f6100c74b5ee2b67aa3446371a
Moses Samuel <mosamuel98@gmail.com>
2017-06-30 19:07:44 -0700

    refactor snake constructor into own file + rename main js file to 'sketch'

--- snake.js
+++ snake.js
@@ -1,15 +1,3 @@
-var s;
-function setup() {
-    createCanvas(600,600);
-    s = new Snake();
-}
-
-function draw() {
-    background(51);
-    s.update();
-    s.show();
-}
-
 // Constructor
 function Snake() {
     this.x = 0;
